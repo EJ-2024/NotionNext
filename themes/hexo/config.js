c10d6a5c629ed663cc87f010d7f8d9c8148ac9f6
EJ-2024 <elijahjia@gmail.com>
2024-10-15 16:47:52 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,10 +2,7 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是一个程序员',
-    'Hi，我是一个打工人',
-    'Hi，我是一个干饭人',
-    '欢迎来到我的博客🎉'
+    '亲爱的家人，主内平安！欢迎来到与光同行，让我们一起跟随主耶稣，经历以福音为中心的全人更新之旅🎉',
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
